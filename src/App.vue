cd32827943888a720d348f90807971d09736cab0
lee-jaewook <jalee4930@gmail.com>
2021-05-24 17:06:31 +0900

    lecture  13

--- src/App.vue
+++ src/App.vue
@@ -5,51 +5,34 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-app-bar-nav-icon @click="drawer = !drawer"/>
+      <site-title :title="title"></site-title>
+      <v-spacer/>
     </v-app-bar>
-
-    <v-main>
+    <v-navigation-drawer app v-model="drawer">
+      <site-menu></site-menu>
+    </v-navigation-drawer>
+    <v-content>
       <router-view/>
-    </v-main>
+    </v-content>
+    <site-footer :footer="footer"></site-footer>
   </v-app>
 </template>
 
 <script>
-
+import SiteTitle from '@/components/title'
+import SiteFooter from '@/components/footer'
+import SiteMenu from '@/components/menu'
 export default {
+  components: { SiteTitle, SiteFooter, SiteMenu },
   name: 'App',
-
-  data: () => ({
-    //
-  })
+  data () {
+    return {
+      drawer: false,
+      items: [],
+      title: '나의 타이틀입니다.',
+      footer: 'footer 입니다.'
+    }
+  }
 }
 </script>
---
+++ src/components/footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-footer app color="primary" dark absolute :footer="footer">
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+      {{ new Date().getFullYear() + ' ' +footer }}
+      </v-col>
+    </v-footer>
+</template>
+<script>
+export default {
+  props: ['footer']
+}
+</script>
